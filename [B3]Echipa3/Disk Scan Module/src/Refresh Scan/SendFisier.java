ab40a92919acd3ed7ca0aa3d8380c0f3d94f04d2
FIIAurelian <FIIAurelian@users.noreply.github.com>
2015-04-27 23:07:21 +0300

    Create SendFisier.java

---
+++ [B3]Echipa3/Disk Scan Module/src/Refresh Scan/SendFisier.java
@@ -0,0 +1,11 @@
+
+public class SendFisier {
+	private ListFisier lista;
+	public SendFisier(ListFisier listaFisier){
+		lista = listaFisier;
+		sendList();
+	}
+	public ListFisier sendList(){
+		return lista;
+	}
+}
