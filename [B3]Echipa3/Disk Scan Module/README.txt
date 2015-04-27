136b07d0217c69f187704e9cb5e38a5bfb9ced90
petru-manea <manea_petre@yahoo.com>
2015-04-27 19:32:46 +0300

    Added Source Files

---
+++ [B3]Echipa3/Disk Scan Module/README.txt
@@ -0,0 +1,1 @@
+Fisierele sursa pentru Modulul Disk-Scan
---
+++ [B3]Echipa3/Disk Scan Module/src/Start Scan/ListFisier.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ip5;
+
+/**
+ *
+ * @author user
+ */
+public class ListFisier {
+    private Fisier[] fisiere;
+    private Serializare salvare;
+    private Deserializare citire;
+    
+    public void addFisier(Fisier fisierul){
+        fisiere[fisiere.length] = fisierul;
+    }
+    
+    public void removeFisier(Fisier fisierul){
+        for(int i = 0; i<fisiere.length; i++)
+            if(fisiere[i].equals(fisierul))
+                for(int j = i; j<fisiere.length-1; j++)
+                    fisiere[j] = fisiere[j+1];
+    }
+    
+    public Fisier[] getList(){
+        return this.fisiere;
+    }
+}
