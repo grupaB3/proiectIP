b842413dc36b643ed8a579352bdb5f5a1426a925
DanteRyuu <dante.ryuuzaki94@gmail.com>
2015-04-27 15:36:39 +0300

    Added menu bar

---
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/menuBar/MenuBar.java
@@ -0,0 +1,10 @@
+package menuBar;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+public class MenuBar extends JMenuBar {
+	JMenu fileMenu = new JMenu("Files");
+	JMenu processMenu = new JMenu("Processes");
+	JMenu infoMenu = new JMenu("Info");
+}
