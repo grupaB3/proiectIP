ce9894e64a9a8a5185aa1608252af65aaabfb34b
DanteRyuu <dante.ryuuzaki94@gmail.com>
2015-05-04 22:33:48 +0300

    Modified split panes

--- [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/OrderFiles.java
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/OrderFiles.java
@@ -18,12 +18,12 @@ public class OrderFiles extends JPanel {
 		JLabel extension = new JLabel("Extension");
 		extension.setPreferredSize(new Dimension(70, 30));
 		JLabel path = new JLabel("Path");
-		path.setPreferredSize(new Dimension(150, 30));
+		path.setPreferredSize(new Dimension(750, 30));
 		
-		JSplitPane splitPane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, name, extension);
+		JSplitPane splitPane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, extension, path);
 	    splitPane1.setDividerSize(2);
 	    splitPane1.setDividerLocation(0.5);
-		JSplitPane splitPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, splitPane1, path);
+		JSplitPane splitPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, name, splitPane1);
 	    splitPane2.setDividerLocation(0.4);
 	    splitPane2.setDividerSize(2);
 		add(splitPane2, BorderLayout.LINE_START);
