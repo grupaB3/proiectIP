2be35668ef5d0ca285a198e3c0db1e1325821466
Foca Razvan <razvan_andrei94@yahoo.com>
2015-05-04 19:55:14 +0300

    Implementare Stergere Fisier

--- [B3]Echipa2/Implementare/Process Check/src/model/SecureDelete.java
+++ [B3]Echipa2/Implementare/Process Check/src/model/SecureDelete.java
@@ -1,7 +1,40 @@
 package model;
 
-public class SecureDelete {
-	public static boolean delete(String nume){
-		return false;}
-	protected void updateFileList(){}
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.security.SecureRandom;
+
+public class SecureDelete
+{
+	public boolean delete(String path) throws IOException{
+		File file = new File(path);
+		if(file.isFile()){
+			long length = file.length();
+			SecureRandom random = new SecureRandom();
+			RandomAccessFile raf = new RandomAccessFile(file,"rws");
+			raf.seek(0);
+			raf.getFilePointer();
+			byte[] data = new byte[64];
+			int position = 0;
+			while(position < length){
+				random.nextBytes(data);
+				raf.write(data);
+				position += data.length;
+			}
+			raf.close();
+			file.delete();
+			return true;
+		}
+		else
+		{
+			if(file.isDirectory()){
+				File[] list = file.listFiles();
+				for(File f:list)
+					delete(f.getAbsolutePath());
+			}
+			file.delete();
+		}
+		return false;
+		}
 }
