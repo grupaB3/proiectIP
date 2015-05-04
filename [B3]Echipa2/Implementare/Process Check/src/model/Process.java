fe66ab453f2c3f0784b18520b8b099614f471c7f
Foca Razvan <razvan_andrei94@yahoo.com>
2015-05-05 00:09:10 +0300

    Update Clasa Process

--- [B3]Echipa2/Implementare/Process Check/src/model/Process.java
+++ [B3]Echipa2/Implementare/Process Check/src/model/Process.java
@@ -1,60 +1,124 @@
 package model;
 
+import java.util.List;
+
 public class Process {
-	private int pid;
-	private String nume;
-	private int threads;
-	private double readBit;
-	private double writeBit;
+
+	private String name;
+	private int PID;
+	private String sessionName;
+	private int sessionNo;
+	private String status;
 	private double memoryUsage;
-	
-	
-	public Process(int pid, String nume,int threads,double readBit,double writeBit,double memoryUsage)
-	{
-		this.pid = pid;
-		this.nume = nume;
-		this.threads= threads;
-		this.readBit = readBit;
-		this.writeBit = writeBit;
+	private double readBites;
+	private double writtenBites;
+	private List<String> readsFromThreads;
+	private List<String> writesToThreads;
+
+	public Process() {
+	}
+
+	public Process(String name, int pID, String sessionName, int sessionNo, String status, double memoryUsage) {
+		super();
+		this.name = name;
+		this.PID = pID;
+		this.sessionName = sessionName;
+		this.sessionNo = sessionNo;
+		this.status = status;
 		this.memoryUsage = memoryUsage;
 	}
 
-	public int getPid() {
-		return pid;
+	public String getName() {
+		return name;
 	}
-	public void setPid(int pid) {
-		this.pid = pid;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getNume() {
-		return nume;
+
+	public int getPID() {
+		return PID;
 	}
-	public void setNume(String nume) {
-		this.nume = nume;
+
+	public void setPID(int pID) {
+		PID = pID;
 	}
-	public int getThreads() {
-		return threads;
+
+	public String getSessionName() {
+		return sessionName;
 	}
-	public void setThreads(int threads) {
-		this.threads = threads;
+
+	public void setSessionName(String sessionName) {
+		this.sessionName = sessionName;
 	}
-	public double getReadBit() {
-		return readBit;
+
+	public int getSessionNo() {
+		return sessionNo;
 	}
-	public void setReadBit(double readBit) {
-		this.readBit = readBit;
+
+	public void setSessionNo(int sessionNo) {
+		this.sessionNo = sessionNo;
 	}
-	public double getWriteBit() {
-		return writeBit;
+	
+	public String getStatus() {
+		return status;
 	}
-	public void setWriteBit(double writeBit) {
-		this.writeBit = writeBit;
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
+
 	public double getMemoryUsage() {
 		return memoryUsage;
 	}
+
 	public void setMemoryUsage(double memoryUsage) {
 		this.memoryUsage = memoryUsage;
 	}
+
+	public double getReadBites() {
+		return readBites;
+	}
+
+	public void setReadBites(double readBites) {
+		this.readBites = readBites;
+	}
+
+	public double getWrittenBites() {
+		return writtenBites;
+	}
+
+	public void setWrittenBites(double writtenBites) {
+		this.writtenBites = writtenBites;
+	}
+
+	public List<String> getReadsFromThreads() {
+		return readsFromThreads;
+	}
+
+	public void setReadsFromThreads(List<String> readsFromThreads) {
+		this.readsFromThreads = readsFromThreads;
+	}
+
+	public List<String> getWritesToThreads() {
+		return writesToThreads;
+	}
+
+	public void setWritesToThreads(List<String> writesToThreads) {
+		this.writesToThreads = writesToThreads;
+	}
+
+	@Override
+	public String toString() {
+		return "Process [name=" + name + ", PID=" + PID + ", sessionName=" + sessionName + ", sessionNo=" + sessionNo + ", memoryUsage="
+				+ memoryUsage + "]";
+	}
 	
-	public void show(){}
+	public String showActivity(){
+		double total = writtenBites + readBites;
+		return "Process [name="+ name + ", PID=" + PID + ", Read(B/sec)=" + readBites + ",Write(B/sec)=" + writtenBites + ",Total(B/sec)="
+				+ total + "]";
+		
+	}
+
 }
