5baa3f67df0b0c34eb99eca4522884c90865241d
UngureanuTeodor <teodor.ungureanu@yahoo.com>
2015-05-09 22:21:28 +0300

    added

--- [B3]Echipa4/Mockup/DiskInvestigator/src/mainPackage/GUI.java
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/mainPackage/GUI.java
@@ -1,9 +1,26 @@
 package mainPackage;
 
 import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 
 public class GUI {
+	
 	public static void main (String[] args) {
+		
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		try {
+			
+			UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceDustCoffeeLookAndFeel");
+			//UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGeminiLookAndFeel");
+			//UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceDustLookAndFeel");
+			//UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceTwilightLookAndFeel");
+			
+		} catch (Exception e) {
+	          System.out.println("Substance Graphite failed to initialize");
+	        }
+		
 		EventQueue.invokeLater(new Runnable() {
 	        
             @Override
