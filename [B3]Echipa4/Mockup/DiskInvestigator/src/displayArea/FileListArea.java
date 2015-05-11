bf6b1712014da58c92bdac68910af7245e8e47be
OanaA <oana.anghelache94@yahoo.com>
2015-05-11 22:26:24 +0300

    sonia_fileDisplayer

--- [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/FileListArea.java
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/FileListArea.java
@@ -2,19 +2,39 @@ package displayArea;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JViewport;
+import javax.swing.table.DefaultTableModel;
 
-import javax.swing.JPanel;
 
-public class FileListArea extends JPanel {
+public class FileListArea extends JScrollPane {
 
 	private static final long serialVersionUID = -2809884889360505234L;
-
+	private String[] columnNames = {"Name","Extension","Size","Status","Path"};
+	private Object[][] data = {
+	        {"row1", "row1","row1", "row1","row1"},
+	        {"row1", "row1","row1", "row1","row1"}};
 	protected FileListArea() {
 		initUI();
 	}
 	
 	private void initUI() {
-		setBackground(Color.orange);
+		//setBackground(Color.orange);
 		setPreferredSize(new Dimension(620, 450));
+		@SuppressWarnings("serial")
+		  DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+
+		   @Override
+		  public boolean isCellEditable(int row, int column) {
+		    return false;
+		  }
+		};
+		 JTable table = new JTable(model);
+		 table.setFillsViewportHeight(true);
+		 table.setForeground(Color.black);
+		 table.setShowGrid(false);
+		 getViewport().add(table);
+		 getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
 	}
 }
