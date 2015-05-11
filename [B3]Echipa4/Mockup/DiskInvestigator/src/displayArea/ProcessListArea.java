4129f78347624f08091fe066c44072dc777b9102
zzcatalin <zz_catalin@yahoo.com>
2015-05-11 21:24:23 +0300

    catalin

--- [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/ProcessListArea.java
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/ProcessListArea.java
@@ -1,11 +1,18 @@
 package displayArea;
 
+
+
+
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JPanel;
+import javax.swing.JTable;
 
-public class ProcessListArea extends JPanel {
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.table.DefaultTableModel;
+
+public class ProcessListArea extends JScrollPane {
 
 	private static final long serialVersionUID = -2809884889360505234L;
 
@@ -16,5 +23,28 @@ public class ProcessListArea extends JPanel {
 	private void initUI() {
 		setBackground(Color.cyan);
 		setPreferredSize(new Dimension(620, 450));
+		
+		String[] columnNames = {"Name", "Session Name", "Pid", "Memory"};
+		Object[][] data = { {"Name1", "Session Name1", "Pid1", "Memory1"},
+							{"Name2", "Session Name2", "Pid2", "Memory2"}};
+
+		@SuppressWarnings("serial")
+		DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+
+			@Override
+		public boolean isCellEditable(int row, int column) {
+				return false;
+  }
+}; 
+	JTable table = new JTable(model); 
+	table.setFillsViewportHeight(true);
+	table.setAutoCreateRowSorter(true);
+	table.setForeground(Color.black);
+	table.setShowGrid(false);
+	table.setShowVerticalLines(true);
+	
+	getViewport().add(table);
+	getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
+		
 	}
 }
