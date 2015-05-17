cc3c30af0c45b6be9f2a7fcc84aaddf0854fdd53
UngureanuTeodor <teodor.ungureanu@yahoo.com>
2015-05-17 11:59:54 +0300

    update

---
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/displayArea/DrawPanel.java
@@ -0,0 +1,25 @@
+package displayArea;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+class DrawPanel extends JPanel {
+	
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D)g;
+		try {
+			
+			BufferedImage bi = ImageIO.read(new File(getClass().getResource("/displayArea/background.png").toURI()));
+			g2d.drawImage(bi, 0, 0, 900, 440, null);
+		} catch (IOException | URISyntaxException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
