80908515876167989308945857df85d097ccea80
Andrei Hălceanu <dante.ryuuzaki94@gmail.com>
2015-05-17 03:06:27 +0300

    Finished buttons

--- [B3]Echipa4/Mockup/DiskInvestigator/src/processToolBar/ProcessScanButton.java
+++ [B3]Echipa4/Mockup/DiskInvestigator/src/processToolBar/ProcessScanButton.java
@@ -1,6 +1,10 @@
 package processToolBar;
 
-import java.awt.Dimension;
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ProcessScanButton extends JButton{
@@ -8,9 +12,11 @@ public class ProcessScanButton extends JButton{
 	private static final long serialVersionUID = 1378373445586830621L;
 
 	protected ProcessScanButton(){
-		super("Process Scan");
-		setPreferredSize(new Dimension(60,40));
-		setMaximumSize(new Dimension(60,40));
-		setMinimumSize(new Dimension(60,40));
+		try {
+		    Image img = ImageIO.read(getClass().getResource("ProcessScan.png"));
+		    this.setIcon(new ImageIcon(img));
+		  } catch (IOException ex) {
+		  }
+		this.setToolTipText("Scan Processes");
 	}
 }
